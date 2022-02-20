fbdb4e9518ff2f47d5efd172c79a84b168c17f5d
Lolhostis <72961722+Lolhostis@users.noreply.github.com>
2022-02-20 14:35:59 +0100

    Ajout des classes du projet JavaFX tetris + documentation

---
+++ Source/app/src/main/java/fr/iut/dut2/tetris/application/src/classes/content/pieces/PieceLDroite.java
@@ -0,0 +1,29 @@
+package fr.iut.dut2.tetris.application.src.classes.content.pieces;
+
+import classes.content.Partie;
+import classes.content.enums.PieceType;
+import classes.content.grille.GrilleBase;
+import classes.content.pieces.point.Point;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Piece de type "LDroite"
+ */
+public class PieceLDroite extends PieceBase {
+
+    /**
+     * Constructeur d'une pièce LDroite
+     *
+     * @param type   type de la pièce
+     * @param x      position de la case centrale sur l'axe des abscisses
+     * @param grille grille de jeu
+     * @param partie partie possédant les attributs nécessaires
+     */
+    public PieceLDroite(PieceType type, int x, @NotNull GrilleBase grille, Partie partie) {
+        super(type, grille, partie);
+        for (int i = 0; i < numPoints - 1; i++) {
+            this.points[i] = new Point(x, i);
+        }
+        this.points[numPoints - 1] = new Point(x + 1, 2);
+    }
+}
