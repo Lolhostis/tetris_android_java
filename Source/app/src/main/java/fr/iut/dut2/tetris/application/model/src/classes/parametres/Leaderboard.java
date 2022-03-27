e652d7e3d11a69260afabe4f7cbbdbf2d4d9a444
MrystAll <msuchet63@gmail.com>
2022-03-27 16:00:36 +0200

    Correction du leaderboard (supression de fonctions non utilisées)

--- Source/app/src/main/java/fr/iut/dut2/tetris/application/model/src/classes/parametres/Leaderboard.java
+++ Source/app/src/main/java/fr/iut/dut2/tetris/application/model/src/classes/parametres/Leaderboard.java
@@ -14,10 +14,6 @@ import android.net.Uri;
 public class Leaderboard implements Serializable {
     private List<Integer> scores = new LinkedList<>();
 
-    public Leaderboard() {
-        //chargerFichier();
-    }
-
     public List<Integer> getScores() {
         return scores;
     }
@@ -39,50 +35,5 @@ public class Leaderboard implements Serializable {
         scores.remove((Integer) value);
         Collections.sort(scores);
         Collections.reverse(scores);
-    }
-
-    public void chargerFichier() {
-        URL s = requireNonNull(this.getClass().getResource("/persistance/Leaderboard.ser"));
-        File f = null;
-        try {
-            f = new File(s.toURI());
-            try {
-                ObjectInputStream reader = new ObjectInputStream(new FileInputStream(f));
-                Integer scoreCharge = (Integer) reader.readObject();
-                while (scoreCharge != null) {
-                    addScore(scoreCharge);
-                    scoreCharge = (Integer) reader.readObject();
-                }
-            } catch (EOFException ex) {
-                ex.printStackTrace();
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("Fichier \"" + f.getName() + "\" non trouvé \n" +
-                    "Chemin : " + f.getPath());
-        } catch (IOException | URISyntaxException | ClassNotFoundException e2) {
-            e2.printStackTrace();
-        }
-    }
-
-    public void sauvegarder() {
-        URL s = requireNonNull(this.getClass().getResource("/persistance/Leaderboard.ser"));
-        File f = null;
-        try {
-            f = new File(s.toURI());
-            if (!f.exists()) {
-                if (!f.createNewFile())
-                    throw new Exception("Le fichier n'a pas été créé correctement.");
-            }
-            ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(f));
-            for (Integer score : scores) {
-                writer.writeObject(score);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Fichier \"" + f.getName() + "\" non trouvé \n" +
-                    "Chemin : " + f.getPath());
-        } catch (Exception e2) {
-            e2.printStackTrace();
-        }
     }
 }
